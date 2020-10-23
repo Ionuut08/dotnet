cb40a3176b75be9f35bd39e6b7144270bc75e2a8
Ionut Feraru <ionut.feraru@info.uaic.ro>
2020-10-23 13:18:30 +0300

    Fourth Laboratory

---
+++ Fourth_Laboratory/API/Controllers/ProductsController.cs
@@ -0,0 +1,78 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Product>> Get() 
+        {
+            return _context.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Product> Get(int id) 
+        {
+            return _context.Products.Find(id);
+        }
+
+        public ActionResult<double> Sum() 
+        {
+            double sum = 0;
+            foreach (Product product in _context.Products) 
+            {
+                sum = sum + product.Price;
+            }
+            return sum;
+        }
+
+        [Route("ValidProducts")]
+
+        public ActionResult<List<Product>> ValidProducts() 
+        {
+            DateTime today = DateTime.Today;
+            List<Product> validProducts = new List<Product>();
+
+            foreach (Product product in _context.Products) 
+            {
+                if (product.ValidTo < today) 
+                {
+                    validProducts.Add(product);
+                }
+            }
+            return validProducts.ToList();
+        }
+
+        [Route("ValidSum")]
+
+        public ActionResult<double> ValidSum() 
+        {
+            DateTime today = DateTime.Today;
+            double sum = 0;
+            foreach (Product product in _context.Products) 
+            {
+                if (product.ValidTo < today) 
+                {
+                    sum = sum + product.Price;
+                }
+            }
+            return sum;
+        }
+    }
+}
